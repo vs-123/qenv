43a04a8ad411ae9665be241b073b7943e971afae
VS-123 <>
2026-03-09 13:16:22 +0000

    main.c: configuration state
    - create =qenv_t= struct to hold the setting

--- src/main.c
+++ src/main.c
@@ -1,14 +1,25 @@
 #include <stdio.h>
 #include <string.h>
 #include <stddef.h>
+#include <stdbool.h>
 
 extern char **environ;
+
+typedef struct
+{
+   char *search_pattern;
+   bool is_verbose_mode;
+} qenv_t;
+
+void
+process_env (qenv_t q)
+{
+   /* TODO */
+}
 
 int
 main (void)
 {
-   printf ("Hello, World!\n");
-
    for (char **env = environ; *env != NULL; env++)
       {
          char *entry = *env;
