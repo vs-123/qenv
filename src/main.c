f7f5df1bc44ead45659f60e2bd3d809397b2c2ba
VS-123 <>
2026-03-09 13:11:51 +0000

    qenv: get env vars

--- src/main.c
+++ src/main.c
@@ -1,8 +1,20 @@
 #include <stdio.h>
+#include <string.h>
+#include <stddef.h>
+
+extern char **environ;
 
 int
 main (void)
 {
    printf ("Hello, World!\n");
+
+   for (char **env = environ; *env != NULL; env++)
+      {
+         char *entry = *env;
+
+         printf ("%s\n", entry);
+      }
+
    return 0;
 }
